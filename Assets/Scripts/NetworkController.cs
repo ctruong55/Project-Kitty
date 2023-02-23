73ad2eb48f4e1c773d0bce4e27772a16120b7f3b
Tom <tomqu2015@gmail.com>
2023-02-23 18:08:19 -0500

    Multiplayer

---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,23 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    public override void OnConnectedToMaster()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
