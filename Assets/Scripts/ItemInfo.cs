235ebd8ee27c28cf4931a2f86a571ba9e1db273e
Dibaa23 <113129269+Dibaa23@users.noreply.github.com>
2023-02-26 01:20:35 -0500

    Working Shop System

--- Assets/Scripts/ItemInfo.cs
+++ Assets/Scripts/ItemInfo.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ItemInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int ItemID;
+    public TMP_Text Pricetxt;
+    public TMP_Text QuantityTxt;
+    public GameObject ShopManager;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    void Update() {
+
+        Pricetxt.text = "Price: $ " + ShopManager.GetComponent<Shop>().shopItems[2,ItemID].ToString();
+        QuantityTxt.text = ShopManager.GetComponent<Shop>().shopItems[3, ItemID].ToString();
     }
 }
