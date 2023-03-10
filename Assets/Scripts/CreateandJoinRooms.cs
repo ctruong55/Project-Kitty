f86903ae4e156c7ce406479a71d01a7b7140f2a8
Dibaa23 <113129269+Dibaa23@users.noreply.github.com>
2023-03-09 19:45:43 -0500

    JoinCreateRooms

---
+++ Assets/Scripts/CreateandJoinRooms.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+
+public class CreateandJoinRooms : MonoBehaviour
+{
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateRoom() 
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Multiplayer");
+    }
+
+}
